2e7da3611aefee5bb5c241ed3c26e8d5b3c854e8
soporteMCM <alberto.s@masconmenos.com.mx>
2026-01-16 22:28:09 -0600

    Actualización de IP

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API_CONFIG = {
-    BASE_URL: "http://18.117.29.228:5000",
+    BASE_URL: "http://3.13.66.5:5000",
     // BASE_URL: "http://192.168.1.2:5000",
 
     // Endpoints de la API
